12919d6bf80f0f7ce2c5047caf01347e51f46758
sebastianmihai01 <mihaisebastian01@gmail.com>
2025-02-12 12:00:22 +0100

    Entry point udpated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,78 +1,34 @@
-import React, { useEffect, useState } from "react";
-import Navigation from "./Navigation";
-import Register from "../authentication/Register";
-import VerifyJWT from "../../hooks/VerifyJWT";
-import VerifyStats from "../../hooks/VerifyStats";
-import Logo from "../../images/mainlogo_full.png"; //S3 reference
-import reducer from "../../redux/reducer";
-import redux from "redux";
-import { Provider } from "react-redux";
+
+import React, { useEffect } from "react";
+import { logger } from "./utils/logger";
+import ErrorBoundary from "./Errors/ErrorBoundary";
+import * as Sentry from "sentry"
+import {BrowserTracing} from "@sentry/tracing"
 
 const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-
-  const handlerLoggedInState = (
-    email: string,
-    token: string,
-    clientId: string,
-    games?: string,
-    wins?: string,
-    created?: string,
-    friends?: string
-  ) => {
-    setIsLoggedIn(true);
-    localStorage.setItem("isLoggedIn", "true");
-    localStorage.setItem("email", email);
-    localStorage.setItem("token", token);
-    localStorage.setItem("clientId", clientId);
-    localStorage.setItem("games", games?.toString() || "0");
-    localStorage.setItem("wins", wins?.toString() || "0");
-    localStorage.setItem("created", created || "0");
-    localStorage.setItem("friends", friends || "");
-  };
 
   useEffect(() => {
-    setIsLoading(true);
-    //avoid double mount
-    try {
-      // iife to set sessions on reload
-      (async () => {
-        const email = await localStorage.getItem("email");
-        const token = await localStorage.getItem("email");
-        const clientId = await localStorage.getItem("email");
-        const value = await VerifyJWT(email, token, clientId);
-        if (value === true) {
-          //cache
-          setIsLoggedIn(true);
-        }
-      })();
+    logger.info("React App has started.");
+  }, []);
 
-      // iife to set stats on reload, since games are updated constantly
-      (async () => {
-        const clientId = await localStorage.getItem("clientId") || ''
-        const data = await VerifyStats(clientId);
-      })();
+  const handleError = () => {
+    try {
+      throw new Error("Simulated error!");
     } catch (error) {
-      setIsLoggedIn(false);
+      logger.error(error.message);
     }
-    setIsLoading(false);
-  }, [VerifyJWT, setIsLoggedIn, setIsLoading]);
+  };
+
+  window.addEventListener("error", (event) => {
+  console.error("Uncaught Error:", event.error);
+  // Log error to an external monitoring service
+});
 
   return (
     <React.Fragment>
-      {!isLoading && !localStorage.getItem("token") && !isLoggedIn && (
-        <div className="flex items-center justify-center h-screen">
-          <div className="w-100 h-full max-h-screen overflow-y-auto">
-            <img className="w-full h-full" src={Logo} />
-          </div>
-          <div className="w-1/3 max-h-screen overflow-y-auto">
-            <Register handler={handlerLoggedInState} />
-          </div>
-        </div>
-      )}
-      {!isLoading && isLoggedIn && <Navigation />}
-      <Navigation />
+      <ErrorBoundary>
+        <LandingPage/>
+      </ErrorBoundary>
     </React.Fragment>
   );
 };
